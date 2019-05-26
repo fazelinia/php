183febf1635eba5dde44abf21fb9798e87f134c8
vahidfazelinia <vahidfazelinia@yahoo.com>
2019-05-26 15:09:04 +0430

    Start a new project to train the students

---
+++ index.php
@@ -0,0 +1,18 @@
+<html>
+<head>
+    <title>Learn PHP</title>
+</head>
+<body>
+    <?php
+        /**
+         * Created by PhpStorm.
+         * User: fazeli
+         * Date: 5/25/19
+         * Time: 10:37 AM
+         */
+        // echo "Hello";
+    phpinfo();
+    ?>
+
+</body>
+</html>
