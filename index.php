5042a2c8b8442339e57b2af71491188de96803db
vahidfazelinia <vahidfazelinia@yahoo.com>
2019-05-26 17:03:30 +0430

    CHANGE

--- index.php
+++ index.php
@@ -11,7 +11,20 @@
          * Time: 10:37 AM
          */
         // echo "Hello";
-    phpinfo();
+//    phpinfo();
+    $mix = array(1 , 2 , array("ali","hasan","hosein"));
+    echo $mix[2];
+    echo $mix[2][1];
+    ?>
+    <pre>
+        <?php
+        print_r($mix);
+        ?>
+        ?>
+    </pre>
+
+    
+    <?php
     ?>
 
 </body>
