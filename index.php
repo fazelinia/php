eb0b99f0c5cc794e0f3ce92bfefffe02d3ea23b7
vahidfazelinia <vahidfazelinia@yahoo.com>
2019-06-12 17:13:14 +0430

    :ribbon: Sql connection

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <html>
 <head>
+    <meta charset="UTF-8">
     <title>Learn PHP</title>
 </head>
 <body>
@@ -23,8 +24,33 @@
         ?>
     </pre>
 
-    
+
     <?php
+        $servername = "localhost";
+        $username = "phpmyadmin";
+        $password = "phpmyadmin";
+        $dbname = "dd";
+
+        // Create connection
+        $conn = new mysqli($servername, $username, $password, $dbname);
+        // Check connection
+        if ($conn->connect_error) {
+            die("Connection failed: " . $conn->connect_error);
+        }
+        //don't show ???? ????
+        $conn->query("SET NAMES 'utf8'");
+        $sql = "SELECT * FROM x_user";
+        $result = $conn->query($sql);
+
+        if ($result->num_rows > 0) {
+            // output data of each row
+            while($row = $result->fetch_assoc()) {
+                echo "user id: " . $row["user_id"]. " <br />" . " - Full Name: " . $row["fullname"]. "<br />" . "&nbsp registerTime: " . $row["registerTime"]. "<br />";
+            }
+        } else {
+            echo "0 results";
+        }
+        $conn->close();
     ?>
 
 </body>
